b3ce67f05313b106f6f897fbcd4fb969148edcbf
Andy Holt <andyholt85@gmail.com>
2018-12-11 12:46:19 -0700

    fleshed out victory and defeat templates a little more

--- app.rb
+++ app.rb
@@ -27,3 +27,8 @@ post '/' do
   
   erb template, :locals => { :colored_pegs => session[:game].colored_pegs, :black_white_pegs => session[:game].black_white_pegs, :turn => session[:game].turn, :secret_code => session[:game].secret_code }
 end
+
+post '/new_game' do
+  session[:game] = Mastermind.new
+  erb :index, :locals => { :colored_pegs => session[:game].colored_pegs, :black_white_pegs => session[:game].black_white_pegs }
+end
