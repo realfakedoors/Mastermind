69e77da4b6a6f51baed2ccaa446f260cfc0bb185
Andy Holt <andyholt85@gmail.com>
2018-12-13 18:17:23 -0700

    added a few comments on style.css and app.rb

--- app.rb
+++ app.rb
@@ -7,11 +7,13 @@ configure do
   enable :sessions
 end
 
+#when the page is first loaded.
 get '/' do
   session[:game] ||= Mastermind.new
   erb :index, :locals => { :colored_pegs => session[:game].colored_pegs, :black_white_pegs => session[:game].black_white_pegs }
 end
-          
+
+#when a guess is submitted.
 post '/' do
   pegs_entered = [ params["first-peg"], params["second-peg"], params["third-peg"], params["fourth-peg"] ]
   session[:game].play(pegs_entered)
@@ -28,6 +30,7 @@ post '/' do
   erb template, :locals => { :colored_pegs => session[:game].colored_pegs, :black_white_pegs => session[:game].black_white_pegs, :turn => session[:game].turn, :secret_code => session[:game].secret_code }
 end
 
+#a route for the 'New Game' button on victory/defeat templates that resets the session.
 post '/new_game' do
   session[:game] = Mastermind.new
   erb :index, :locals => { :colored_pegs => session[:game].colored_pegs, :black_white_pegs => session[:game].black_white_pegs }
